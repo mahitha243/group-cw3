261fbd6bd99973d035d75a047abda2a59c8a1942
mahitha243 <mahithadasari24@gmail.com>
2022-02-28 21:02:57 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cst3145
+this is my cw1 of cst3145
---
+++ lessons.js
@@ -0,0 +1,124 @@
+let lessonsArray = [
+
+{
+    id: 1,
+                        title: "Math" ,
+                        location: "London" ,
+                        price: 100,
+                        availablespace: 10,
+                        image: "image/math.png",
+                imageWidth: 200, //making the image expand
+               // availableInventory: 5
+},
+{
+    id: 2,
+                        title: "science" ,
+                        location: "London" ,
+                        price: 150,
+                        availablespace: 8,
+                        image: "image/science.png",
+                imageWidth: 200, //making the image expand
+                //availableInventory: 5
+},
+{
+    id: 3,
+                        title: "Crafts" ,
+                        location: "Harrow" ,
+                        price: 180,
+                        availablespace: 15,
+                        image: "image/crafts.png",
+                imageWidth: 200, //making the image expand
+                //availableInventory: 5
+},
+{
+    id: 4,
+    title: "English" ,
+    location: "Ilford" ,
+    price: 95,
+    availablespace: 10,
+    image: "image/English.png",
+imageWidth: 200, //making the image expand
+//availableInventory: 5
+},
+{
+    id: 5,
+    title: "Arts" ,
+    location: "Harrow" ,
+    price: 80,
+    availablespace: 10,
+    image: "image/arts.png",
+imageWidth: 200, //making the image expand
+//availableInventory: 5
+},
+{
+    id: 6,
+    title: "Musics" ,
+    location: "Harrow" ,
+    price: 75,
+    availablespace: 12,
+    image: "image/Musics.png",
+imageWidth: 200, //making the image expand
+//availableInventory: 5,
+},
+{
+    id: 7,
+    title: "IT" ,
+    location: "Harrow" ,
+    price: 85,
+    availablespace: 11,
+    image: "image/IT.png",
+imageWidth: 200, //making the image expand
+availableInventory: 5,
+    },
+    {
+        id: 8,
+    title: "history" ,
+    location: "Harrow" ,
+    price: 90,
+    availablespace: 7,
+    image: "image/History.png",
+imageWidth: 200, //making the image expand
+availableInventory: 5,
+    },
+    {
+        id: 9,
+    title: "Geography" ,
+    location: "Harrow" ,
+    price: 60,
+    availablespace: 5,
+    image: "image/Geography.png",
+imageWidth: 200, //making the image expand
+availableInventory: 5,
+    },
+    {
+        id: 10,
+    title: "Physics" ,
+    location: "Harrow" ,
+    price: 70,
+    availablespace: 5,
+    image: "image/Physics.png",
+imageWidth: 200, //making the image expand
+availableInventory: 5,
+    },
+{
+    id: 11,
+    title: "Drama" ,
+    location: "Harrow" ,
+    price: 105,
+    availablespace: 2,
+    image: "image/drama.png",
+imageWidth: 200, //making the image expand
+availableInventory: 5,
+},
+{
+id: 12,
+    title: "Calligraphy" ,
+    location: "Harrow" ,
+    price: 55,
+    availablespace: 5,
+    image: "image/calligraphy.png",
+imageWidth: 200, //making the image expand
+//availableInventory: 5,
+}
+
+]
