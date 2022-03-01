4eb3d8838daad780af3c9c9fb039588e9cb8315a
mahitha243 <mahithadasari24@gmail.com>
2022-03-01 12:29:54 +0000

    added fetch

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,8 @@ var cacheFiles = [
     'lessons.js',
     'service-worker.js',
     'afterSchoolapp.webmanifest',
+    
+
 ];
 
 self.addEventListener('install', (e) => {
@@ -18,12 +20,39 @@ self.addEventListener('install', (e) => {
     )
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (e)  {
-            return r
+//self.addEventListener('fetch', function (e) {
+   // e.respondWith(
+      //  caches.match(e.request).then(function (r)  {
+           // return r
 
-        })
-    )
+       // })
+ //   )
+//});
+self.addEventListener('fetch', (e) => {
+console.log('[Service Worker] Fetched resource '+e.request.url);
 });
-
+self.addEventListener('fetch', function (e) {
+e.respondWith(
+// check if the cache has the file
+caches.match(e.request).then(function (r) {
+console.log('[Service Worker] Fetching resource: '
++ e.request.url);
+// 'r' is the matching file if it exists in the cache
+return r
+})
+);
+});
+self.addEventListener('fetch', function (e) {
+e.respondWith(
+caches.match(e.request).then(function (r) {
+// Download the file if it is not in the cache,
+return r || fetch(e.request).then(function (response) {
+// add the new file to cache
+return caches.open(cacheName).then(function (cache) {
+cache.put(e.request, response.clone());
+return response;
+});
+});
+})
+);
+})
